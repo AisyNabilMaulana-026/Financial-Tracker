068aa1ae89c4fd06ab57d00773aaa285db0341bb
AisyNabilMaulana-026 <nabilmaulana250606@gmail.com>
2025-11-29 22:37:46 +0700

    Implement function to add expense transactions

--- src/tambah_pengeluaran.c
+++ src/tambah_pengeluaran.c
@@ -1,1 +1,33 @@
+void tambahUangKeluar() {
+    bersihkanLayar();
+    tampilBanner("TAMBAH PENGELUARAN");
 
+    printf("Kategori : ");
+    getchar();
+    fgets(daftarTransaksi[jumlahTransaksi].kategori, 50, stdin);
+    daftarTransaksi[jumlahTransaksi].kategori[strcspn(daftarTransaksi[jumlahTransaksi].kategori, "\n")] = 0;
+
+    printf("Jumlah   : ");
+    scanf("%d", &daftarTransaksi[jumlahTransaksi].jumlah);
+
+    if (daftarTransaksi[jumlahTransaksi].jumlah > saldoSaatIni) {
+        aturWarna(12);
+        tulisPelan("Saldo tidak cukup! Transaksi dibatalkan.\n", 10);
+        aturWarna(7);
+        return;
+    }
+
+    strcpy(daftarTransaksi[jumlahTransaksi].jenis, "Keluar");
+    ambilTanggal(daftarTransaksi[jumlahTransaksi].tanggal);
+
+    saldoSaatIni -= daftarTransaksi[jumlahTransaksi].jumlah;
+    jumlahTransaksi++;
+
+    simpanData();
+
+    aturWarna(10);
+    tulisPelan("\nPengeluaran berhasil dicatat!\n", 15);
+    aturWarna(14);
+    printf("Tanggal: %s\n", daftarTransaksi[jumlahTransaksi - 1].tanggal);
+    aturWarna(7);
+}
